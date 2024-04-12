aa0bebcb18ab6d814e96b003a2a37123ed431a8d
pauljsymonds <paul.j.symonds@gmail.com>
2024-04-12 22:40:16 +0100

    Updated for Windows to create folder relative to where running

--- whdownload.py
+++ whdownload.py
@@ -3421,17 +3421,11 @@ def download_file(url):
 
 
 if __name__ == "__main__":
+    # On Mac & Linux, creates my-amiga-files folder relative to where run from.
+    filePath = Path(os.getcwd() + os.sep + 'my-amiga-files' + os.sep)
 
-    if __name__ == "__main__":
-        # On Mac & Linux, creates folder in home directory.
-        if os.name == 'posix':
-                filePath = pathlib.Path.home().joinpath('my-amiga-games')
-        # on Windows creates folder relative to where the code is run
-        if os.name == 'nt':
-                filePath = os.getcwd()
-
-        if not os.path.exists(filePath):
-                os.makedirs(filePath)
+    if not os.path.exists(filePath):
+        os.makedirs(filePath)
 
     os.chdir(filePath)
     print("filePath is %s " % filePath)
